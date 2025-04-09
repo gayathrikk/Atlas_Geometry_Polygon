d4788902d9bff2a205343e040caca1bd8db3b557
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-04-09 11:24:29 +0530

    Update Atlas_Geometry_Polygon.java

--- src/test/java/Sanity/Atlas/Atlas_Geometry_Polygon.java
+++ src/test/java/Sanity/Atlas/Atlas_Geometry_Polygon.java
@@ -69,7 +69,7 @@ public class Atlas_Geometry_Polygon {
 	        }
 	        WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='email']")));
 	        if (emailInput != null && emailInput.isDisplayed()) {
-	            emailInput.sendKeys("softwaretestingteam9@gmail.com");
+	            emailInput.sendKeys("teamsoftware457@gmail.com");
 	            System.out.println("Email was entered successfully.");
 	        } else {
 	           System.out.println("Email was not entered.");
@@ -232,7 +232,7 @@ public class Atlas_Geometry_Polygon {
 			
 			 
 			 try {
-		  		    WebElement contributor= wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//input[@type='radio'])[2]")));
+		  		    WebElement contributor= wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//input[@type='radio'])[3]")));
 		  		    contributor.click();
 		  		    System.out.println("contributor selected successfully.");
 		  		    Thread.sleep(2000);
@@ -316,6 +316,7 @@ public class Atlas_Geometry_Polygon {
 			clickElementByXpath("//button[text()='Delete']", "Delete button");
 			clickElementByXpath("//a[@title='Save']", "save");
 			System.out.println("Polygon region deleted successfully");
+			clickElementByXpath("//a[@title='Close']", "Close");
 		}
 		
 		
